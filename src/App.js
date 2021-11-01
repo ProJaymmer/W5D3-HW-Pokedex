845708e3a356d5f7c752dc59311c9ecbf9a00837
Jay Nguyen <jay@Jays-iMac.local>
2021-11-01 09:18:49 -0700

    W5D3-HW-Pokedex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Route, Redirect, Link, Switch } from 'react-router-dom';
+import Pokedex from './Pokedex';
+import MyTeam from './MyTeam';
+import Pokemon from './Pokemon';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [pokemons, setPokemon] = useState();
+
+	const makeApiCall = (pokemonUrl) =>
+		fetch(pokemonUrl)
+			.then((response) => response.json())
+			.then((data) => setPokemon(data.results));
+
+	useEffect(() => {
+		const pokemonUrl = 'https://pokeapi.co/api/v2/pokemon/';
+		makeApiCall(pokemonUrl);
+	}, []);
+
+	console.log(pokemons);
+
+	return (
+		<div className='App'>
+			<nav>
+				<h1>Pokedex Project</h1>
+
+				<Link to='/'>Pokedex</Link>
+				<br />
+
+				<Link to='/myteam'>My Team</Link>
+			</nav>
+
+			<Switch>
+				<Route exact path='/' render={() => <Pokedex pokemons={pokemons} />} />
+				<Route path='/pokedex' exact render={() => <Redirect to='/' />} />
+				<Route
+					path='/pokemon/:id'
+					exact
+					render={(routerProps) => (
+						<Pokemon {...routerProps} pokemons={pokemons} />
+					)}
+				/>
+				<Route path='/myteam' exact component={MyTeam} />
+				<Redirect to='/' />
+			</Switch>
+		</div>
+	);
 }
 
 export default App;
